18b18ab92a417f1cf0ae1a37e7af329478cc4281
mjitkop <77814122+mjitkop@users.noreply.github.com>
2023-06-02 20:43:53 -0400

    Ready for first public release
    This is it! Hopefully, it will work fine for everybody.

--- README.md
+++ README.md
@@ -1,2 +1,149 @@
 # Channels DVR Monitor Channel Lineups
- Sens notifications when channel lineups change on a Channels DVR server
+ Send notifications when channel lineups change on a Channels DVR server
+ 
+ Script usage:
+ 
+ 1. Display the help text:
+ 
+ > python channels_dvr_monitor_channels.py -h
+ 
+ usage: channels_dvr_monitor_channels.py [-h] [-e EMAIL_ADDRESS] [-f FREQUENCY] [-i IP_ADDRESS] [-p PORT_NUMBER]
+                                         [-P PASSWORD] [-r RECIPIENT_ADDRESS] [-t TEXT_NUMBER] [-v]
+
+Monitor a Channels DVR server for changes in channel lineups.
+
+options:
+  -h, --help            show this help message and exit
+  -e EMAIL_ADDRESS, --email_address EMAIL_ADDRESS
+                        Email address to use as the sender. Not required if just monitoring on the screen. Use with
+                        -P, and either -r or -t (or both).
+  -f FREQUENCY, --frequency FREQUENCY
+                        Frequency of queries sent to the Channels DVR server, in minutes. Not required. Default: 30.
+                        Minimum: 5.
+  -i IP_ADDRESS, --ip_address IP_ADDRESS
+                        IP address of the Channels DVR server. Not required. Default: 127.0.0.1
+  -p PORT_NUMBER, --port_number PORT_NUMBER
+                        Port number of the Channels DVR server. Not required. Default: 8089
+  -P PASSWORD, --password PASSWORD
+                        Password to log in to the email account. Use with -e.
+  -r RECIPIENT_ADDRESS, --recipient_address RECIPIENT_ADDRESS
+                        Email address of the recipient. Not required if -t is specified. Use with -e and -P. May be
+                        the same as -e. May be used with -t too.
+  -t TEXT_NUMBER, --text_number TEXT_NUMBER
+                        Cell phone number to send a text to in the format: <10 digits>@<SMS gateway>. Not required if
+                        -r is specified. Use with -e and -P. May be used with -r too.
+  -v, --version         Print the version number and exit the program.
+
+If no options are specified, use the default URL http://127.0.0.1:8089 to query the Channels DVR server, and just
+print information on the screen. If the -e argument is specified, you must provide at least either -r or -t, or both.
+ 
+---
+
+2. Run the script on the same machine that is running Channels DVR and choose to do visual monitoring only, no notifications, at the default frequency of 30 minutes:
+
+>  python channels_dvr_monitor_channels.py
+
+Using Channels DVR URL: http://127.0.0.1:8089/devices.
+Checking for channel lineup changes every 30 minutes.
+
+Visual monitoring only, no email or text will be sent.
+
+Last check: 2023-05-26 23:53:53
+added channels   : {}
+modified channels: {}
+removed channels : {}
+
+Next check: 2023-05-27 00:23:53
+
+---
+
+3. Same thing but check for changes every 15 minutes:
+
+> python channels_dvr_monitor_channels.py -f 15
+
+Using Channels DVR URL: http://127.0.0.1:8089/devices.
+Checking for channel lineup changes every 15 minutes.
+
+Visual monitoring only, no email or text will be sent.
+
+Last check: 2023-05-26 23:56:50
+added channels   : {}
+modified channels: {}
+removed channels : {}
+
+Next check: 2023-05-27 00:11:50
+
+---
+
+4. Now the Channels DVR is running on a different machine and its IP address is 192.168.0.155, use the default port number:
+
+> python channels_dvr_monitor_channels.py -f 15 -i 192.168.0.155
+
+Using Channels DVR URL: http://192.168.0.155:8089/devices.
+Checking for channel lineup changes every 15 minutes.
+
+Visual monitoring only, no email or text will be sent.
+
+Last check: 2023-05-26 23:59:05
+added channels   : {}
+modified channels: {}
+removed channels : {}
+
+Next check: 2023-05-27 00:14:05
+
+---
+
+5. Back to defaults but now add email notifications:
+
+> python channels_dvr_monitor_channels.py -e EMAIL_ADDRESS -P PASSWORD -r RECIPIENT_ADDRESS
+
+Using Channels DVR URL: http://127.0.0.1:8089/devices.
+Checking for channel lineup changes every 30 minutes.
+
+An email will be sent to RECIPIENT_ADDRESS when changes are detected.
+
+Last check: 2023-05-27 00:02:16
+added channels   : {}
+modified channels: {}
+removed channels : {}
+
+Next check: 2023-05-27 00:32:16
+
+*** Notes:
+
+EMAIL_ADDRESS = address of the email account that will be used to send the email
+
+PASSWORD = the password to log in to that email account. In the case of Yahoo! Mail and Gmail, you will have to create an app password to be used exclusively by this script. If using Outlook.com, the normal account password will work.
+
+RECIPIENT_ADDRESS = email address where to send the email to. It can be the same as EMAIL_ADDRESS (send email to yourself)
+
+---
+
+6. You can add text notification:
+
+> python channels_dvr_monitor_channels.py -e EMAIL_ADDRESS -P PASSWORD -r RECIPIENT_ADDRESS -t NUMBER@SMS_GATEWAY
+
+Using Channels DVR URL: http://127.0.0.1:8089/devices.
+Checking for channel lineup changes every 30 minutes.
+
+An email will be sent to EMAIL_ADDRESS when changes are detected.
+A text message will be sent to NUMBER@SMS_GATEWAY when changes are detected.
+
+Last check: 2023-05-27 00:12:59
+added channels   : {}
+modified channels: {}
+removed channels : {}
+
+Next check: 2023-05-27 00:42:59
+
+*** Notes:
+
+NUMBER = 10 digits of cell phone number
+SMS_GATEWAY = see https://en.wikipedia.org/wiki/SMS_gateway for the SMS gateway used by your cell phone carrier
+
+EMAIL_ADDRESS from Yahoo! Mail has been tested successfully to send text messages to a Verizon number.
+EMAIL_ADDRESS from Outlook.com: text messages are very delayed in my tests (more than 12 hours!) so not recommended
+
+A Gmail account has NOT been tested at all as the sender of either emails or text messages, but support for it is present in the script. 
+ 
+
--- channels_dvr_monitor_channels.py
+++ channels_dvr_monitor_channels.py
@@ -1,7 +1,7 @@
 """
 Author: Gildas Lefur (a.k.a. "mjitkop" in the Channels DVR forums)
 
-Description: This script monitors the channel lineups in a Channels DVR server.
+Description: This script monitors the channel lineups on a Channels DVR server.
              If changes are detected, it may send an email and/or text message
              as specified in the input arguments.
 
@@ -12,7 +12,7 @@ For bug reports and support, go to:
 https://community.getchannels.com/t/python-script-to-be-notified-of-channel-changes/36464?u=mjitkop
 
 Version History:
-- 2023.05.25.2348: Initial version of the script.
+- 2023.06.01.2301: Initial public release.
 """
 
 ################################################################################
@@ -33,17 +33,17 @@ from email.mime.text import MIMEText
 #                                                                              #
 ################################################################################
 
-DEFAULT_PORT_NUMBER = '8089'
-EMAIL_SUBJECT       = 'Channels DVR: changes in channel lineups'
-LOOPBACK_ADDRESS    = '127.0.0.1'
-SMTP_PORT           = 587
-SMTP_SERVER_ADDRESS = {
+DEFAULT_PORT_NUMBER  = '8089'
+EMAIL_SUBJECT        = 'Channels DVR: changes in channel lineups'
+LOOPBACK_ADDRESS     = '127.0.0.1'
+SMTP_PORT            = 587
+SMTP_SERVER_ADDRESS  = {
                         'gmail'  : 'smtp.gmail.com', 
                         'outlook': 'smtp-mail.outlook.com', 
                         'yahoo'  : 'smtp.mail.yahoo.com'
-                      }
-TEXT_SUBJECT        = 'Channels'
-VERSION             = '2023.05.25.2348'
+                       }
+TEXT_SUBJECT         = 'Channels DVR'
+VERSION              = '2023.06.01.2301'
 
 ################################################################################
 #                                                                              #
@@ -57,22 +57,23 @@ class ChannelsDVRSource:
         self.source = source
         self.name = self.source['FriendlyName']
         self.current_channels  = self._get_current_channels()
+        self.current_channel_count = len(self.current_channels)
         self.previous_channels = self.current_channels
+        self.previous_channel_count = len(self.previous_channels)
         self.added_channels    = {}
         self.removed_channels  = {}
-        self.modified_channels = {}
 
     def _get_current_channels(self):
         '''Extract the channels from the provided source.'''
         current_channels = {}
 
         for channel in self.source['Channels']:
-            number = channel['GuideNumber']
-            name   = channel['GuideName']
-            current_channels[number] = name
-
+            channel_number = channel['GuideNumber']
+            channel_name   = channel['GuideName']
+            current_channels[channel_name] = channel_number
+        
         return current_channels
-
+        
     def _get_added_channels(self):
         '''
         Compare the previous and current channel lineups and return the 
@@ -82,10 +83,10 @@ class ChannelsDVRSource:
 
         previous_set = set(self.previous_channels)
         current_set  = set(self.current_channels)
-        new_channel_numbers = list(current_set.difference(previous_set))
+        new_channel_names = list(current_set.difference(previous_set))
         
-        for number in new_channel_numbers:
-            added_channels[number] = self.current_channels[number]
+        for name in new_channel_names:
+            added_channels[name] = self.current_channels[name]
 
         return added_channels
 
@@ -98,39 +99,25 @@ class ChannelsDVRSource:
         
         previous_set = set(self.previous_channels)
         current_set  = set(self.current_channels)
-        removed_channel_numbers = list(previous_set.difference(current_set))
+        removed_channel_names = list(previous_set.difference(current_set))
         
-        for number in removed_channel_numbers:
-            removed_channels[number] = self.previous_channels[number]
+        for name in removed_channel_names:
+            removed_channels[name] = self.previous_channels[name]
 
         return removed_channels
 
-    def _get_modified_channels(self):
-        '''
-        Compare the channel numbers and channel names in the previous
-        channels and the current channels. Return the list of channels
-        whose names are different for the same channel numbers.
-        '''
-        modified_channels = {}
-
-        for channel_number, previous_name in self.previous_channels.items():
-            current_name = self.current_channels.get(channel_number, None)
-            if current_name and current_name != previous_name:
-                modified_channels[channel_number] = (previous_name, current_name)
-
-        return modified_channels
-        
     def update(self, device):
         '''
         Given the updated current channels retrieved from the Channels DVR server
         (in device), update all internal attributes accordingly.
         '''
-        self.source = device
-        self.current_channels  = self._get_current_channels()
-        self.added_channels    = self._get_added_channels()
-        self.modified_channels = self._get_modified_channels()
-        self.removed_channels  = self._get_removed_channels()
-        self.previous_channels = self.current_channels
+        self.source                 = device
+        self.current_channels       = self._get_current_channels()
+        self.added_channels         = self._get_added_channels()
+        self.removed_channels       = self._get_removed_channels()
+        self.current_channel_count  = len(self.current_channels)
+        self.previous_channel_count = len(self.previous_channels)
+        self.previous_channels      = self.current_channels
 
 ################################################################################
 #                                                                              #
@@ -146,93 +133,75 @@ def create_sources(devices):
     '''
     return [ChannelsDVRSource(device) for device in devices]
 
-def format_message_for_email(added_channels, modified_channels, removed_channels):
+def format_message_for_email(server_version, sources, channel_count_message):
     '''Generate a string that is easy to read in an email.'''
-    message = ""
+    message = f'Channels DVR version: {server_version}\n'
     
-    if added_channels:
-        message += "------ New channels ------\n\n"
-        
-        for source_name, channels in added_channels.items():
-            message += f'{source_name}\n'
-            sorted_numbers = list(channels.keys())
-            sorted_numbers.sort()
-            
-            for number in sorted_numbers:
-                name = channels[number]
-                message += f'    {number} : {name}\n'
-                
+    for source in sources:
+        if source.added_channels or source.removed_channels:
             message += '\n'
-            
-    if modified_channels:
-        message += "------ Modified channels ------\n\n"
+            message += f'{source.name}:\n'
         
-        for source_name, channels in modified_channels.items():
-            message += f'{source_name}\n'
-            sorted_numbers = list(channels.keys())
-            sorted_numbers.sort()
+        if source.removed_channels:
+            sorted_names = list(source.removed_channels.keys())
+            sorted_names.sort()
+        
+            for name in sorted_names:
+                number = source.removed_channels[name]
+                message += f'- {name} ({number})\n'
             
-            for number in sorted_numbers:
-                previous_name = channels[number][0]
-                new_name      = channels[number][1]
-                message += f'    {number} : {previous_name} -> {new_name}\n'
-                
+        if source.added_channels:
+            sorted_names = list(source.added_channels.keys())
+            sorted_names.sort()
+            
+            for name in sorted_names:
+                number = source.added_channels[name]
+                message += f'+ {name} ({number})\n'
+            
+        if source.added_channels or source.removed_channels:
             message += '\n'
-        
-    if removed_channels:
-        message += "------ Removed channels ------\n\n"
-        
-        for source_name, channels in removed_channels.items():
-            message += f'{source_name}\n'
-            sorted_numbers = list(channels.keys())
-            sorted_numbers.sort()
-            
-            for number in sorted_numbers:
-                name = channels[number]
-                message += f'    {number} : {name}\n'
-                
-            message += '\n'
-            
+            channel_count_diff = source.current_channel_count - source.previous_channel_count
+            signed_number = "{:+}".format(channel_count_diff)
+            message += f'{source.name} channel count: {source.current_channel_count} ({signed_number})\n'
+    
     return message
 
-def format_message_for_sms(added_channels, modified_channels, removed_channels):
+def format_message_for_sms(added_channels, removed_channels):
     '''Generate a short message that is suitable for a text/SMS'''
     message = ""
     
     if added_channels:
-        message += "*New* "
-        message = add_numbers_to_sms_message_and_return(message, added_channels)
+        message += "\n*New* "
+        message = add_number_of_channels_to_sms_message(message, added_channels)
             
-    if modified_channels:
-        message += "\n*Changed* "
-        message = add_numbers_to_sms_message_and_return(message, modified_channels)
-        
     if removed_channels:
         message += "\n*Removed* "
-        message = add_numbers_to_sms_message_and_return(message, removed_channels)
+        message = add_number_of_channels_to_sms_message(message, removed_channels)
             
     return message
     
-def add_numbers_to_sms_message_and_return(message, channel_list):
+def add_number_of_channels_to_sms_message(message, channel_list):
     '''
-    Parse the channel list and add source name + list of numbers to the message.
+    Parse the channel list and add source name + number of channels to the message.
     Return the message when done.
     '''
     for source_name, channels in channel_list.items():
         source_name = source_name.split(' ')[0] # Only keep the first word
         message += f'{source_name}: '
-        sorted_numbers = list(channels.keys())
-        sorted_numbers.sort()
+        number_channels = len(channels)
         
-        for number in sorted_numbers:
-            message += f'{number}, '
+        if number_channels == 1:
+            message += '1 channel. '
+        else:
+            message += f'{number_channels} channels. '
             
-        # Remove the last comma and space
-        message = message[:-2]
-        
-        message += ' '
-    
     return message
+
+def get_channels_dvr_version(ip_address, port_number):
+    '''Return the current version of the Channels DVR server'''
+    url = f'http://{ip_address}:{port_number}/status'
+    
+    return requests.get(url).json()['version']
 
 def get_email_provider(sender_address):
     '''Given the email address as user@provider.com, return the provider.'''
@@ -245,11 +214,28 @@ def get_message_type(destination):
     '''
     provider = get_email_provider(destination)
     
-    type = 'an email'
-    if not provider in SMTP_SERVER_ADDRESS.keys():
-        type = 'a text'
+    message_type = 'a text'
+    if provider in SMTP_SERVER_ADDRESS.keys():
+        message_type = 'an email'
         
-    return type
+    return message_type
+    
+def create_message_with_new_channel_counts(sources, modified_sources):
+    '''
+    Generate a string that contains the names of all the modified sources
+    and their respective new channel counts, as well as the count differences
+    compared with the previous channel counts.
+    '''
+    
+    message = ""
+    
+    for source in sources:
+        if source.name in modified_sources: 
+            channel_count_diff = source.current_channel_count - source.previous_channel_count
+            signed_number = "{:+}".format(channel_count_diff)
+            message += f'{source.name} channel count: {source.current_channel_count} ({signed_number})\n'
+
+    return message
 
 def send_message(sender_address, password, subject, destination, message):
     '''Send a message from the given email account to the specified destination.'''
@@ -352,15 +338,15 @@ if __name__ == "__main__":
             print(f'A password must be specified with -P to log in to the {sender_address} account!')
             sys.exit()
         if not (recipient_address or text_number):
-            print('Use the -r and/or the -t option with -e!')
+            print('Use the -r and/or the -t option in combination with -e!')
             sys.exit()
             
     if recipient_address and not sender_address:
-        print('Use the -e option with -r!')
+        print('Use the -e and -P options with -r!')
         sys.exit()
         
     if text_number and not sender_address:
-        print('Use the -e option with -t!')
+        print('Use the -e and -P options with -t!')
         sys.exit()
         
     if frequency < 5:
@@ -369,9 +355,9 @@ if __name__ == "__main__":
         
     # All good. Let's go!
 
-    server_url = f'http://{ip_address}:{port_number}/devices'
+    url_server_devices = f'http://{ip_address}:{port_number}/devices'
     print('')
-    print(f'Using Channels DVR URL: {server_url}.')
+    print(f'Using Channels DVR URL: {url_server_devices}.')
     print(f'Checking for channel lineup changes every {frequency} minutes.')
     print('')
     
@@ -388,51 +374,58 @@ if __name__ == "__main__":
     print('')        
     
     # Retrieve information about the sources from the Channels DVR server
-    sources = create_sources(requests.get(server_url).json())
+    sources = create_sources(requests.get(url_server_devices).json())
     
     while True:
         # Check for changes in the channel lineups in all sources
-        added_channels    = {}
-        modified_channels = {}
-        removed_channels  = {}
+        added_channels   = {}
+        removed_channels = {}
         
+        server_version = get_channels_dvr_version(ip_address, port_number)
+
         # Get the current list of devices from the Channels DVR server
-        devices = requests.get(server_url).json()
+        devices = requests.get(url_server_devices).json()
 
         # Update the sources with this newly retrieved list of devices, and
-        # generate the list of added, modified, and removed channels.
+        # generate the list of added, and removed channels.
         for source in sources:
             for device in devices:
                 if device['FriendlyName'] == source.name:
                     break
                     
             source.update(device)
-            
+
             if source.added_channels:
                 added_channels[source.name] = source.added_channels
-            if source.modified_channels:
-                modified_channels[source.name] = source.modified_channels
+
             if source.removed_channels:
                 removed_channels[source.name] = source.removed_channels
 
         current_time = datetime.now()
         next_check_time = current_time + timedelta(minutes=frequency)
-        print("Last check:", current_time.strftime("%Y-%m-%d %H:%M:%S"))
-        print(f'added channels   : {added_channels}')
-        print(f'modified channels: {modified_channels}')
-        print(f'removed channels : {removed_channels}')
-        print('')
+        print( 'Last check      :', current_time.strftime("%Y-%m-%d %H:%M:%S"))
+        print(f'Server version  : {server_version}')
+        print(f'added channels  : {added_channels}')
+        print(f'removed channels: {removed_channels}')
         
-        if sender_address and (added_channels or modified_channels or removed_channels):
+        modified_sources = list(added_channels.keys())
+        modified_sources.extend(removed_channels.keys())
+        if modified_sources:
+            print('')
+            channel_count_message = create_message_with_new_channel_counts(sources, modified_sources)
+            print(channel_count_message)
+            print('')
+        
+        if sender_address and modified_sources:
             if recipient_address:
-                message = format_message_for_email(added_channels, modified_channels, removed_channels)
+                message = format_message_for_email(server_version, sources, channel_count_message)
                 send_message(sender_address, password, EMAIL_SUBJECT, recipient_address, message)
                 
             if text_number:
-                message = format_message_for_sms(added_channels, modified_channels, removed_channels)
+                message = format_message_for_sms(added_channels, removed_channels)
                 send_message(sender_address, password, TEXT_SUBJECT, text_number, message)
         
-        print("Next check:", next_check_time.strftime("%Y-%m-%d %H:%M:%S"))
+        print('Next check      :', next_check_time.strftime("%Y-%m-%d %H:%M:%S"))
         print('')
         
         time.sleep(frequency * 60)
